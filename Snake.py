e203507c95c197d454571a7b29e149735a82e61e
Valentin Deumier <valentin.deumier@etu.minesparis.psl.eu>
2023-11-07 16:36:00 +0100

    Fixed checkers background

--- Snake.py
+++ Snake.py
@@ -11,6 +11,11 @@ nbr_hor_squares = 20
 nbr_ver_squares = 15
 sq_width = sc_width/nbr_hor_squares
 sq_height = sc_height/nbr_ver_squares
+coord_X = [i*sq_width for i in range(nbr_hor_squares)]
+coord_Y = [i*sq_height for i in range(nbr_ver_squares)]
+
+print(coord_X)
+print(coord_Y)
 
 
 screen = pg.display.set_mode((sc_width, sc_height))
@@ -20,15 +25,12 @@ flag = True
 while flag:
 
     screen.fill(bg_color)                                       #background color
-    left = 0
-    top = 0
 
-    for i in range(nbr_hor_squares//2):                            #checkers setup
-        for j in range(nbr_ver_squares//2):           
-            rect = pg.Rect(left, top, sq_width, sq_height)
-            pg.draw.rect(screen, brighter_col, rect)
-            top += 2*sq_height
-        left += 2*sq_width
+    for i in range(nbr_hor_squares):                            #checkers setup
+        for j in range(nbr_ver_squares):    
+            if (i+j)%2 == 1:       
+                rect = pg.Rect(coord_X[i], coord_Y[j], sq_width, sq_height)
+                pg.draw.rect(screen, brighter_col, rect)
 
     for event in pg.event.get():
 
